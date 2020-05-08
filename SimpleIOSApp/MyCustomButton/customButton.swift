8386145576877fa9c1c81026dc2dec27690baa4a
brainstorm <sekulovski.nikola@gmail.com>
2020-05-08 10:31:07 -0500

    commit from Xcode

---
+++ SimpleIOSApp/MyCustomButton/customButton.swift
@@ -0,0 +1,23 @@
+//
+//  customButton.swift
+//  SimpleIOSApp
+//
+//  Created by Nikola Sekulovski on 5/7/20.
+//  Copyright © 2020 Nikola Sekulovski. All rights reserved.
+//
+
+import UIKit
+
+class MyCustomButton : UIButton
+{
+    override init(frame: CGRect) {
+        
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    
+}
